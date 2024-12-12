56053e922336f54ab3845b1c26a7ade4cf0f0eea
zach <zdicklin@gmail.com>
2024-12-11 22:04:20 -0700

    Play a 440hz tone

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 use std::f64::consts::PI;
 
 struct SinWave {
@@ -21,7 +22,7 @@ impl Iterator for SinWave {
     }
 }
 
-fn main() -> std::io::Result<()> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Hello world!");
     let sin_wave = SinWave::new(440.0);
 
@@ -31,5 +32,36 @@ fn main() -> std::io::Result<()> {
         let amplitude = sin_wave_iter.next().unwrap_or(0.0);
         println!("{}", amplitude);
     }
+
+    let host = cpal::default_host();
+    let device = host
+        .default_output_device()
+        .expect("Did not find default output device");
+    let config = device.default_output_config().unwrap();
+    dbg!(&config);
+
+    let err_fn = |err| eprintln!("an error occurred on stream: {}", err);
+    let stream_config: cpal::StreamConfig = config.into();
+
+    let stream = device.build_output_stream(
+        &stream_config,
+        move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
+            write_data(data, 2, &mut sin_wave_iter)
+        },
+        err_fn,
+        None,
+    )?;
+    stream.play()?;
+
+    std::thread::sleep(std::time::Duration::from_millis(1000));
     Ok(())
+}
+
+fn write_data(output: &mut [f32], channels: usize, next_sample: &mut dyn Iterator<Item = f64>) {
+    for frame in output.chunks_mut(channels) {
+        let sample = next_sample.next().unwrap();
+        for s in frame.iter_mut() {
+            *s = sample as f32;
+        }
+    }
 }
